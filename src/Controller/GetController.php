ab604a88f0893e842dbc263ff0a406d11d563d00
Diego Altamirano <diegutor@gmail.com>
2020-10-27 22:22:10 +0100

    Update Twig dependency in GetController class

--- src/Controller/GetController.php
+++ src/Controller/GetController.php
@@ -5,44 +5,17 @@ namespace Mochilo\Controller;
 use Mochilo\Config;
 use Mochilo\Data;
 use Mochilo\Helper;
-use Twig_Environment;
+use Twig\Environment;
 
 class GetController implements ControllerInterface
 {
-    /**
-     * @var Twig_Environment
-     */
     protected $twig;
-
-    /**
-     * @var array
-     */
     protected $config;
-
-    /**
-     * @var Data
-     */
     protected $data;
-
-    /**
-     * @var int
-     */
     protected $code = 200;
-
-    /**
-     * @var Helper
-     */
     protected $helper;
 
-    /**
-     * MisterProper constructor.
-     *
-     * @param Twig_Environment $twig
-     * @param Config $config
-     * @param Data $data
-     * @param Helper $helper
-     */
-    public function __construct(Twig_Environment $twig, Config $config, Data $data, Helper $helper)
+    public function __construct(Environment $twig, Config $config, Data $data, Helper $helper)
     {
         $this->twig = $twig;
         $this->config = $config;
@@ -50,9 +23,6 @@ class GetController implements ControllerInterface
         $this->helper = $helper;
     }
 
-    /**
-     * @return int
-     */
     public function getCode(): int
     {
         return $this->code;
